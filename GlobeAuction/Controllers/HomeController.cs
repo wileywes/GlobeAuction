1287ec55e75e0589e434349fb338ff7add38ed5b
Wes <williams.wes@gmail.com>
2016-10-22 22:03:10 -0400

    make home anonymous

--- GlobeAuction/Controllers/HomeController.cs
+++ GlobeAuction/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace GlobeAuction.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
