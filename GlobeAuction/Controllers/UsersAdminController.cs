e99ca070c4e5d95f1def2490586b8571656c74ab
Wes <williams.wes@gmail.com>
2017-03-10 13:02:48 -0500

    Users delete

--- GlobeAuction/Controllers/UsersAdminController.cs
+++ GlobeAuction/Controllers/UsersAdminController.cs
@@ -17,9 +17,9 @@ namespace GlobeAuction.Controllers
     {
         public UsersAdminController()
         {
-            context = new ApplicationDbContext();
-            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-            RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            db = new ApplicationDbContext();
+            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
         }
 
         public UsersAdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
@@ -30,10 +30,8 @@ namespace GlobeAuction.Controllers
 
         public UserManager<ApplicationUser> UserManager { get; private set; }
         public RoleManager<IdentityRole> RoleManager { get; private set; }
-        public ApplicationDbContext context { get; private set; }
-
-        //
-        // GET: /Users/
+        public ApplicationDbContext db { get; private set; }
+        
         public async Task<ActionResult> Index()
         {
             //must to ToList() to close out the DataReader from Users
@@ -44,72 +42,7 @@ namespace GlobeAuction.Controllers
                 RoleNames = u.Roles.Select(r => RoleManager.FindById(r.RoleId).Name).ToList()
             }).ToList());
         }
-
-        //
-        // GET: /Users/Details/5
-        public async Task<ActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var user = await UserManager.FindByIdAsync(id);
-            AddUserEditControlInfo(user);
-            return View(user);
-        }
-
-        /*
-        //
-        // GET: /Users/Create
-        public async Task<ActionResult> Create()
-        {
-            //Get the list of Roles
-                        AddUserEditControlInfo(null);
-            return View();
-        }
-
-        //
-        // POST: /Users/Create
-        [HttpPost]
-        public async Task<ActionResult> Create(RegisterViewModel userViewModel, string RoleId)
-        {
-                        AddUserEditControlInfo(null);
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser();
-                user.UserName = userViewModel.UserName;
-                var adminresult = await UserManager.CreateAsync(user, userViewModel.Password);
-
-                //Add User Admin to Role Admin
-                if (adminresult.Succeeded)
-                {
-                    if (!String.IsNullOrEmpty(RoleId))
-                    {
-                        //Find Role Admin
-                        var role = await RoleManager.FindByIdAsync(RoleId);
-                        var result = await UserManager.AddToRoleAsync(user.Id, role.Name);
-                        if (!result.Succeeded)
-                        {
-                            ModelState.AddModelError("", result.Errors.First().ToString());
-                            return View();
-                        }
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError("", adminresult.Errors.First().ToString());
-                    return View();
-
-                }
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View();
-            }
-        }
-        */
-
+                
         //
         // GET: /Users/Edit/1
         public async Task<ActionResult> Edit(string id)
@@ -193,58 +126,51 @@ namespace GlobeAuction.Controllers
             ViewBag.AvailableRoles = new MultiSelectList(availableRoles, "RoleId", "RoleName", myRoles);
         }
 
-        ////
-        //// GET: /Users/Delete/5
-        //public async Task<ActionResult> Delete(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    var user = await context.Users.FindAsync(id);
-        //    if (user == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(user);
-        //}
+        //
+        // GET: /Users/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = await UserManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new ApplicationUserWithRoleNames
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                RoleNames = user.Roles.Select(r => RoleManager.FindById(r.RoleId).Name).ToList()
+            };
+            return View(viewModel);
+        }
 
-        ////
-        //// POST: /Users/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> DeleteConfirmed(string id)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        if (id == null)
-        //        {
-        //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //        }
+        //
+        // POST: /Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
 
-        //        var user = await context.Users.FindAsync(id);
-        //        var logins = user.Logins;
-        //        foreach (var login in logins)
-        //        {
-        //            context.UserLogins.Remove(login);
-        //        }
-        //        var rolesForUser = await IdentityManager.Roles.GetRolesForUserAsync(id, CancellationToken.None);
-        //        if (rolesForUser.Count() > 0)
-        //        {
+                var user = await UserManager.FindByIdAsync(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
 
-        //            foreach (var item in rolesForUser)
-        //            {
-        //                var result = await IdentityManager.Roles.RemoveUserFromRoleAsync(user.Id, item.Id, CancellationToken.None);
-        //            }
-        //        }
-        //        context.Users.Remove(user);
-        //        await context.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    else
-        //    {
-        //        return View();
-        //    }
-        //}
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
