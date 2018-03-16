b4d338b5e534914b87177414edbc826024b340bc
Wes <williams.wes@gmail.com>
2018-03-15 21:44:31 -0400

    revenue report

---
+++ GlobeAuction/Controllers/ReportsController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using GlobeAuction.Models;
+using Microsoft.AspNet.Identity;
+using GlobeAuction.Helpers;
+using System.Web;
+using System.IO;
+
+namespace GlobeAuction.Controllers
+{
+    [Authorize(Roles = AuctionRoles.CanAdminUsers)]
+    public class ReportsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult AllRevenueByType()
+        {
+            var paidInvoices = db.Invoices
+                .Include(i => i.AuctionItems)
+                .Include(i => i.StoreItemPurchases)
+                .Include("StoreItemPurchases.StoreItem")
+                .Where(i => i.IsPaid)
+                .ToList();
+            
+            var allBidders = db.Bidders
+                .Include(b => b.AuctionGuests)
+                .Include(b => b.StoreItemPurchases)
+                .Include("StoreItemPurchases.StoreItem")
+                .ToList();
+
+            var model = new AllRevenueByTypeReportModel
+            {
+                AuctionItems = paidInvoices.Sum(i => i.AuctionItems.Sum(a => a.WinningBid.GetValueOrDefault(0))),
+                BidderTickets = allBidders.Sum(b => b.AuctionGuests.Sum(g => g.TicketPricePaid.GetValueOrDefault(0))),
+                RaffleTicketsViaRegistration = allBidders.Sum(b => b.StoreItemPurchases.Where(sip => sip.StoreItem.IsRaffleTicket).Sum(sip => sip.PricePaid.GetValueOrDefault(0))),
+                StoreSalesViaRegistration = allBidders.Sum(b => b.StoreItemPurchases.Where(sip => !sip.StoreItem.IsRaffleTicket).Sum(sip => sip.PricePaid.GetValueOrDefault(0))),
+                RaffleTicketsViaStore = paidInvoices.Sum(i => i.StoreItemPurchases.Where(sip => sip.StoreItem.IsRaffleTicket).Sum(sip => sip.PricePaid.GetValueOrDefault(0))),
+                StoreSalesViaStore = paidInvoices.Sum(b => b.StoreItemPurchases.Where(sip => !sip.StoreItem.IsRaffleTicket).Sum(sip => sip.PricePaid.GetValueOrDefault(0))),
+            };
+
+            return View(model);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GlobeAuction/Models/ReportsModels.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlobeAuction.Models
+{
+    public class AllRevenueByTypeReportModel
+    {
+        [Display(Name = "Event Tickets")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal BidderTickets { get; set; }
+
+        [Display(Name = "Raffle Tickets Purchased in Registration")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal RaffleTicketsViaRegistration { get; set; }
+
+        [Display(Name = "Raffle Tickets Purchased in Store")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal RaffleTicketsViaStore { get; set; }
+
+        [Display(Name = "Store Sales Purchased in Registration")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal StoreSalesViaRegistration { get; set; }
+
+        [Display(Name = "Store Sales Purchased in Store")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal StoreSalesViaStore { get; set; }
+
+        [Display(Name = "Auction Items")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal AuctionItems { get; set; }
+
+        [Display(Name = "Total Raised")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Total
+        {
+            get { return BidderTickets + RaffleTicketsViaRegistration + RaffleTicketsViaStore + StoreSalesViaRegistration + StoreSalesViaStore + AuctionItems; }
+        }
+    }
+}
