f481bb9c72e8bf7cd02e0610277c81f982c30a0b
Wes Desktop <williams.wes@gmail.com>
2025-03-14 08:22:54 -0400

    user purging queries

--- GlobeAuction/App_Data/Purge bogus users.sql
+++ GlobeAuction/App_Data/Purge bogus users.sql
@@ -1,11 +1,25 @@
-﻿select u.Email, u.EmailConfirmed,
+﻿--see accounts that have no roles assigned
+select u.Email, u.EmailConfirmed,
   (select count(*) from AspNetUserRoles r where r.UserId=u.Id) as RoleCount
 from AspNetUsers u
 where (select count(*) from AspNetUserRoles r where r.UserId=u.Id) = 0
 
+--remove accounts that have no roles assigned
 delete u
 from AspNetUsers u
 where (select count(*) from AspNetUserRoles r where r.UserId=u.Id) = 0
+
+
+--see accounts that have not logged in for 2+ years
+select u.Email, u.EmailConfirmed,
+  (select count(*) from AspNetUserRoles r where r.UserId=u.Id) as RoleCount
+from AspNetUsers u
+where u.LastLogin < dateadd(year, -2, getutcdate())
+
+--remove accounts that have no roles assigned
+delete u
+from AspNetUsers u
+where u.LastLogin < dateadd(year, -2, getutcdate())
 
 
 select *
